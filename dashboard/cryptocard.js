8e3264241d64b8e155b1a7a1023d347b40e2ccd0
hagenwan <hagenwan@gmail.com>
2022-04-06 18:34:59 +0100

    fix for setTimeout

--- dashboard/cryptocard.js
+++ dashboard/cryptocard.js
@@ -65,10 +65,13 @@ async function getData(btcURL) {
 }
 
 plotGraph("btcLine","https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30&interval=daily", "btcPrice");
-plotGraph("ethLine","https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=30&interval=daily", "ethPrice");
-plotGraph("solLine","https://api.coingecko.com/api/v3/coins/solana/market_chart?vs_currency=usd&days=30&interval=daily", "solPrice");
-plotGraph("dotLine","https://api.coingecko.com/api/v3/coins/polkadot/market_chart?vs_currency=usd&days=30&interval=daily", "dotPrice");
+// plotGraph("ethLine","https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=30&interval=daily", "ethPrice");
+// plotGraph("solLine","https://api.coingecko.com/api/v3/coins/solana/market_chart?vs_currency=usd&days=30&interval=daily", "solPrice");
+// plotGraph("dotLine","https://api.coingecko.com/api/v3/coins/polkadot/market_chart?vs_currency=usd&days=30&interval=daily", "dotPrice");
 
+setTimeout(plotGraph,500,"ethLine","https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=30&interval=daily", "ethPrice");
+setTimeout(plotGraph,1000,"solLine","https://api.coingecko.com/api/v3/coins/solana/market_chart?vs_currency=usd&days=30&interval=daily", "solPrice");
+setTimeout(plotGraph,1500,"dotLine","https://api.coingecko.com/api/v3/coins/polkadot/market_chart?vs_currency=usd&days=30&interval=daily", "dotPrice");
 /* randomly pick a quote to display*/
 titleQuotes = ['"Buy high, sell low, easier done than said."','"I am not and financial advidor, that being said..."','"When your taxi driver tells you to buy bitcoin, shill them your shitcoin instead."','"Trust me bruv, it literally cant go tits up"','"When there is blood on the street, oh shit I better sell"'];
 document.getElementById("heroTitle").innerHTML = titleQuotes[Math.floor(Math.random() * titleQuotes.length)];
